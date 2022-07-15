f6d3cb6bcc8e4dd3b55d80d8ebe3d4d9c123e4e1
KimHyunLo <leead016@gmail.com>
2022-07-15 09:49:20 +0900

    mainIMG

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import PostList from "../PostList/PostList";
 import styles from "./Main.module.css";
+import mainIMG from "./test.jpg";
 
 export default function Main(): JSX.Element {
   return (
     <>
-      <div className={styles.main}></div>
+      <div className={styles.main}>
+        <img src={mainIMG} alt="mainIMG" />
+      </div>
       <PostList />
     </>
   );
